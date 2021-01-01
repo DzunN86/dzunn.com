73651f1b6a6d80c3687b1f0bf7901d91a68f5935
DzunN86 <nurroinz@gmail.com>
2021-01-01 15:12:37 +0700

    disable about section

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,7 +38,7 @@ const IndexPage = ({ data }) => {
         <Hero content={data.hero.edges} />
         {/* Articles is populated via Medium RSS Feed fetch */}
         <Articles />
-        <About content={data.about.edges} />
+        {/* <About content={data.about.edges} /> */}
         <Interests content={data.interests.edges} />
         <Projects content={data.projects.edges} />
         <Contact content={data.contact.edges} />
