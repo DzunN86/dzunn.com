f732ee08aa47dccf5221d08b5e0a86579c49c902
dzun-diyo <nurroinz@gmail.com>
2023-04-13 05:56:07 +0700

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,4 +1,4 @@
-import PageTransition from "@/components/PageTransition";
+import PageTransition from "../components/PageTransition";
 import MainLayout from "@/layouts/MainLayout";
 
 export default function about() {
