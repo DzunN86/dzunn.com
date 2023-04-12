8cbdf33f12d7f5ef2389079be17638464f2b6985
dzun-diyo <nurroinz@gmail.com>
2023-04-13 06:00:51 +0700

    Update styles.ts

--- src/theme/styles.ts
+++ src/theme/styles.ts
@@ -1,7 +1,4 @@
 import { mode } from "@chakra-ui/theme-tools";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const globalStyles = {
   colors: {
@@ -13,10 +10,6 @@ export const globalStyles = {
     global: (props: any) => ({
       body: {
         bg: mode("white", "gray.900")(props),
-        fontFamily: inter.style.fontFamily,
-      },
-      html: {
-        fontFamily: inter.style.fontFamily,
       },
     }),
   },
