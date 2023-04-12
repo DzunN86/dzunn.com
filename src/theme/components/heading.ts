ff6d5b3d7313009f5c0174982936b3f6d7eb68af
dzun-diyo <nurroinz@gmail.com>
2023-04-13 06:00:49 +0700

    Update heading.ts

--- src/theme/components/heading.ts
+++ src/theme/components/heading.ts
@@ -1,12 +1,8 @@
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export const headingStyles = {
   components: {
     Heading: {
       baseStyle: {
-        fontFamily: inter.style.fontFamily,
+        fontFamily: "Inter",
       },
     },
   },
