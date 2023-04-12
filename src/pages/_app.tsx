2e93ec8a2ca345e602003415dc4d78e3aa5290ac
dzun-diyo <nurroinz@gmail.com>
2023-04-13 06:04:36 +0700

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,11 @@
 import theme from "@/theme";
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <main className={inter.className}>
-        <Component {...pageProps} />
-      </main>
+      <Component {...pageProps} />
     </ChakraProvider>
   );
 }
