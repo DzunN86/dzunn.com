37ad83cf4883f104a1d988bf458d93e69386304e
Dzun <nurroinz@gmail.com>
2023-06-16 11:16:57 +0700

    Update MainLayout.tsx

--- layouts/MainLayout.tsx
+++ layouts/MainLayout.tsx
@@ -1,5 +1,5 @@
 import Navbar from "@/components/Navbar";
-import { Box, Container, VStack } from "@chakra-ui/react";
+import { Box, Container, Image, VStack } from "@chakra-ui/react";
 import { PropsWithChildren } from "react";
 import PageTransition from "@/components/PageTransition";
 import Footer from "./Footer";
@@ -8,28 +8,18 @@ type Props = PropsWithChildren<{}>;
 
 export default function MainLayout({ children }: Props) {
   return (
-    <Box>
-      <Container
-        display="flex"
-        maxW={{ base: "container.sm", md: "container.lg" }}
-        minH="100vh"
-        px="0"
-        position="relative"
-      >
+    <Box position="relative">
+      <Box position="absolute" top="-300" left="0">
+        <Image src="/bg-gradient.avif" alt="Background Gradient" w="full" h="full" objectFit="cover" />
+      </Box>
+      <Container display="flex" flexDirection="column" maxW={{ base: "container.sm", md: "container.xl" }} minH="100vh" px="0" position="relative">
         <VStack flex={1} alignItems="stretch" w="full">
           <Navbar />
-          <VStack
-            pt={{ base: "2rem", md: "5rem" }}
-            spacing={16}
-            w="full"
-            alignItems="stretch"
-            as="main"
-            px={{ base: "1rem", md: "2rem" }}
-          >
+          <VStack pt={{ base: "2rem", md: "5rem" }} spacing={16} w="full" alignItems="stretch" as="main" px={{ base: "1rem", md: "2rem" }}>
             <PageTransition>{children}</PageTransition>
           </VStack>
-          <Footer />
         </VStack>
+        <Footer />
       </Container>
     </Box>
   );
