b40ebf5a9d78cdad082a892e9ef4a8d65a1c5d9c
Dzun <nurroinz@gmail.com>
2023-06-16 11:17:27 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,21 @@
 /* eslint-disable react/no-unescaped-entities */
+import HeroImage from "@/components/HeroImage";
 import { PageSEO } from "@/components/SEO";
 import MainLayout from "@/layouts/MainLayout";
 import { getBlurDataURL } from "@/utils/blurDataURL";
+import { Grid, GridItem, HStack, Heading, Show, Text, useColorModeValue } from "@chakra-ui/react";
 import NextImage from "next/image";
-import {
-  Box,
-  Grid,
-  GridItem,
-  HStack,
-  Heading,
-  Show,
-  Text,
-  useColorMode,
-} from "@chakra-ui/react";
 
 export default function Home({ posts }: any) {
-  const { colorMode, toggleColorMode } = useColorMode();
   return (
     <MainLayout>
-      <PageSEO
-        title="Dzun Nurroin"
-        description="Dzun Nurroin's personal blog"
-      />
-      <Grid templateColumns="repeat(12, 1fr)" gap={6} alignItems="center">
-        <GridItem colSpan={{ base: 12, md: 9 }}>
+      <PageSEO title="Dzun Nurroin" description="Dzun Nurroin's personal blog" />
+      <Grid templateColumns="repeat(12, 1fr)" gap={{ base: 5, md: "2rem" }} alignItems="center">
+        <GridItem colSpan={{ base: 12, md: 7 }}>
           <HStack spacing={5}>
             <Show below="md">
               <NextImage
-                src={colorMode === "light" ? "/logo.svg" : "/logo-light.svg"}
+                src={useColorModeValue("/logo.svg", "/logo-light.svg")}
                 alt="Dzun Nurroin"
                 placeholder="blur"
                 blurDataURL={getBlurDataURL(700, 475)}
@@ -43,11 +31,12 @@ export default function Home({ posts }: any) {
           <Text mt="1.5rem" fontSize="lg">
             Likes Cats 😸 but doesn't hate 🐼
           </Text>
-          <Text mt="1.5rem" fontSize="xl">
-            a software engineer specialized in Frontend Development creating web
-            based application from landing page and company profile, to internal
-            dashboard and information system.
+          <Text mt="1.5rem" fontSize={{ base: "md", md: "xl" }}>
+            a Software Engineer specialized in Frontend Development creating web based application from landing page and company profile, to internal dashboard and information system.
           </Text>
+        </GridItem>
+        <GridItem colSpan={{ base: 12, md: 5 }} display={{ base: "none", lg: "block" }}>
+          <HeroImage />
         </GridItem>
       </Grid>
     </MainLayout>
