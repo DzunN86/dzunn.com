2bda780e10d22974b8b47407b957be1213ef2653
Dzun <nurroinz@gmail.com>
2023-06-16 11:16:53 +0700

    Update Footer.tsx

--- layouts/Footer.tsx
+++ layouts/Footer.tsx
@@ -3,26 +3,27 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <Box as="footer" pb="3rem" px={{ base: "1rem", md: "2rem" }} pt={{ base: "2rem", md: "5rem" }}>
+    <Box as="footer" pb="2rem" px={{ base: "1rem", md: "2rem" }} pt={{ base: "2rem", md: "5rem" }}>
       <VStack spacing={4} justifyContent="center" alignItems="center">
-        <Text fontSize="md">Made by Dzun Nurroin</Text>
-        <HStack spacing={5} alignItems="center" justifyContent="space-between">
+        <Text fontSize="sm">Made by Dzun Nurroin</Text>
+        <HStack spacing={{ base: 5, md: 6 }} alignItems="center" justifyContent="space-between">
+          <Text fontSize="sm">Build with</Text>
           <Link href="https://nextjs.org" isExternal>
             <HStack spacing={2} alignItems="center">
-              <Image src={useColorModeValue("/nextjs-dark.png", "/nextjs.png")} alt="Github" maxW="1.3rem" />
-              <Text fontSize="md">Next.js</Text>
+              <Image src={useColorModeValue("/nextjs-dark.png", "/nextjs.png")} alt="Github" maxW="1.2rem" />
+              <Text fontSize="sm">Next.js</Text>
             </HStack>
           </Link>
           <Link href="https://github.com/DzunN86" isExternal>
             <HStack spacing={2} alignItems="center">
-              <Image src="/github.png" alt="Github" maxW="1.4rem" />
-              <Text fontSize="md">Github</Text>
+              <Image src="/github.png" alt="Github" maxW="1.3rem" />
+              <Text fontSize="sm">Github</Text>
             </HStack>
           </Link>
           <Link href="https://vercel.com" isExternal>
             <HStack spacing={2} alignItems="center">
               <Image src={useColorModeValue("/vercel-dark.svg", "/vercel.svg")} alt="Github" maxW="1.2rem" />
-              <Text fontSize="md">Vercel</Text>
+              <Text fontSize="sm">Vercel</Text>
             </HStack>
           </Link>
         </HStack>
