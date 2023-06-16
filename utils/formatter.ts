a7d9fd64ebdadc86d0c8592917fcbdbe7d3a6672
Dzun <nurroinz@gmail.com>
2023-06-16 11:18:11 +0700

    Create formatter.ts

---
+++ utils/formatter.ts
@@ -0,0 +1,4 @@
+export const dateFormatter = (date: string) => {
+  const options: Intl.DateTimeFormatOptions = { year: "numeric", month: "long", day: "numeric" };
+  return new Date(date).toLocaleDateString("en-US", options);
+};
