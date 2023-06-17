0c520c508a2f3b99ee32fae41544997787f4c5c2
Dzun <nurroinz@gmail.com>
2023-06-17 16:02:48 +0700

    Analytics Vercel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import theme from "@/theme";
 import { GlobalStyle } from "@/theme/GlobalStyle";
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <GlobalStyle>
         <Component {...pageProps} />
+        <Analytics />
       </GlobalStyle>
     </ChakraProvider>
   );
