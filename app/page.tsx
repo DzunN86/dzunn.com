44f7ae35c975c0b8f1407e330cab06fcad814f21
Dzun <nurroinz@gmail.com>
2024-03-15 18:41:45 +0700

    ui: social link

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
+import BlurImage from "@/components/BlurImage";
 import { Container } from "@/components/Container";
-import { GitHubIcon, InstagramIcon, LinkedInIcon, TwitterIcon } from "@/components/SocialIcons";
+import { DribbbleIcon, GitHubIcon, InstagramIcon, LinkedInIcon, TwitterIcon } from "@/components/SocialIcons";
 import SocialLink from "@/components/SocialLink";
-import Image from "next/image";
 import image1 from "@/images/photos/image-1.jpg";
 import image2 from "@/images/photos/image-2.jpg";
 import image3 from "@/images/photos/image-3.jpg";
 import image4 from "@/images/photos/image-4.jpg";
 import image5 from "@/images/photos/image-5.jpg";
 import clsx from "clsx";
-import BlurImage from "@/components/BlurImage";
 
 function Photos() {
   let rotations = ["rotate-2", "-rotate-2", "rotate-2", "rotate-2", "-rotate-2"];
@@ -27,6 +26,7 @@ function Photos() {
               sizes="(min-width: 640px) 50vw, 100vw"
               // "(min-width: 640px) 18rem, 11rem"
               className="absolute inset-0"
+              placeholder="blur"
             />
           </div>
         ))}
@@ -40,15 +40,15 @@ export default function Home() {
     <>
       <Container className="mt-9">
         <div className="max-w-2xl">
-          <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">Frontend Engineer, Amateur Photographer</h1>
+          <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">Frontend Engineer, and Amateur UI/UX Designer</h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             a Software Engineer specialized in Frontend Development creating web based application from landing page and company profile, to internal dashboard and information system.
           </p>
           <div className="mt-6 flex gap-6">
-            <SocialLink href="https://twitter.com/vinzvinci" aria-label="Follow on Twitter" icon={TwitterIcon} />
-            <SocialLink href="https://www.instagram.com/vinzvinci/" aria-label="Follow on Instagram" icon={InstagramIcon} />
-            <SocialLink href="https://github.com/vinzvinci" aria-label="Follow on GitHub" icon={GitHubIcon} />
-            <SocialLink href="https://www.linkedin.com/in/dzun-n/" aria-label="Follow on LinkedIn" icon={LinkedInIcon} />
+            <SocialLink href="https://github.com/DzunN86" aria-label="Follow on GitHub" icon={GitHubIcon} />
+            <SocialLink href="https://www.linkedin.com/in/dzun-n/" aria-label="Follow on LinkedIn" icon={LinkedInIcon} target="_blank" />
+            <SocialLink href="https://www.instagram.com/hifumi.design/" aria-label="Follow on Instagram" icon={InstagramIcon} />
+            <SocialLink href="https://dribbble.com/dzunn86" aria-label="Follow on Dribbble" icon={DribbbleIcon}  />
           </div>
         </div>
       </Container>
