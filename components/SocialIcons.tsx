305058473a41fb9999121d1af85f2bac4e5d699e
Dzun <nurroinz@gmail.com>
2024-03-20 08:11:30 +0700

    Update SocialIcons.tsx

--- components/SocialIcons.tsx
+++ components/SocialIcons.tsx
@@ -43,3 +43,14 @@ export function DribbbleIcon(props: React.ComponentProps<"svg">) {
     </svg>
   );
 }
+
+export function MailIcon(props: React.ComponentProps<"svg">) {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
+      <path
+        fillRule="evenodd"
+        d="M6 5a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H6Zm.245 2.187a.75.75 0 0 0-.99 1.126l6.25 5.5a.75.75 0 0 0 .99 0l6.25-5.5a.75.75 0 0 0-.99-1.126L12 12.251 6.245 7.187Z"
+      />
+    </svg>
+  )
+}
