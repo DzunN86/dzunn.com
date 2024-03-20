ef84560de6243c0adcd9ec8de0ecf6a8512fc064
Dzun <nurroinz@gmail.com>
2024-03-20 08:09:54 +0700

    Create index.ts

---
+++ constant/index.ts
@@ -0,0 +1,18 @@
+export const menuSchema = [
+  {
+    title: "About",
+    href: "/about",
+  },
+  {
+    title: "Blog",
+    href: "/blog",
+  },
+  {
+    title: "Projects",
+    href: "/projects",
+  },
+  {
+    title: "Short",
+    href: "/short",
+  },
+];
